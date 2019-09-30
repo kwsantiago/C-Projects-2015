698f11593dfb91a2a69a771fb787b4e8449e320a
Kyle Santiago <kwsantiago@mail.usf.edu>
2019-09-30 00:13:12 -0400

    C++

---
+++ Horoscope-2019/main.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    cout << "Horoscope 2019 by Kyle Santiago\n";
+    cout << "Horoscope information provided by www.sunsigns.org\n\n";
+    cout << "1: January\n2: February\n3: March\n4: April\n5: May\n6: June\n7: July\n8: August\n9: September\n10: October\n11: November\n12: December\n";
+    int month;
+    cout << "\nPlease enter the month you were born in: ";
+    cin >> month;
+    cout << "Please enter the day you were born: ";
+    int day;
+    cin >> day;
+    cout << "\nHoroscope \n\n";
+    //Aquarius
+    if (month == 1 && (day >= 21 && day <= 31))
+    {
+        cout << "The 2019 horoscope predicts that the Aquarius star sign will be optimistic this year. Be ready to accept new opportunities that come knocking on your doors. You will be serious when it comes to romantic alliances. 2019 is an auspicious year for single Aquarians to get married. When it comes to your job, you will do pretty well, thanks to your wonderful social skills. And be sure to take care of your mental health with some meditation and mindful spiritual healing.";
+        system("PAUSE");
+    }
+    //Aquarius
+    if (month == 2 && (day >= 1 && day <= 19))
+    {
+        cout << "The 2019 horoscope predicts that the Aquarius star sign will be optimistic this year. Be ready to accept new opportunities that come knocking on your doors. You will be serious when it comes to romantic alliances. 2019 is an auspicious year for single Aquarians to get married. When it comes to your job, you will do pretty well, thanks to your wonderful social skills. And be sure to take care of your mental health with some meditation and mindful spiritual healing.";
+        system("PAUSE");
+    }
+    //Pisces
+    if (month == 2 && (day >= 20 && day <= 28))
+    {
+        cout << "The Pisces sun sign in 2019 might be a bit confused between the reality of life and their creative imagination. And you are the only one who can decide how to balance the both for a happy and good life. Do not make promises that you cannot keep in 2019. Be it dating someone new or starting a new business venture, make sure you fulfill your commitments. Mentally, you need to de-clutter your mind with some spiritual healing and meditation.";
+        system("PAUSE");
+    }
+    //Pisces
+    if (month == 3 && (day >= 1 && day <= 20))
+    {
+        cout << "The Pisces sun sign in 2019 might be a bit confused between the reality of life and their creative imagination. And you are the only one who can decide how to balance the both for a happy and good life. Do not make promises that you cannot keep in 2019. Be it dating someone new or starting a new business venture, make sure you fulfill your commitments. Mentally, you need to de-clutter your mind with some spiritual healing and meditation.";
+        system("PAUSE");
+    }
+    //Aries
+    if (month == 3 && (day >= 21 && day <= 31))
+    {
+        cout << "The Aries zodiac sign will have an excellent time in 2019! This is going to be one lucky year for you in all aspects of your life. Physically and mentally you will be fit and strong. When it comes to love and romance, you will be sure of what you want out of life. In your profession or business, you will be creative and come up with new ideas. 2019 is a good year to be social and enjoy quality time with friends and family.";
+        system("PAUSE");
+    }
+    //Aries
+    if (month == 4 && (day >= 1 && day <= 19))
+    {
+        cout << "The Aries zodiac sign will have an excellent time in 2019! This is going to be one lucky year for you in all aspects of your life. Physically and mentally you will be fit and strong. When it comes to love and romance, you will be sure of what you want out of life. In your profession or business, you will be creative and come up with new ideas. 2019 is a good year to be social and enjoy quality time with friends and family.";
+        system("PAUSE");
+    }
+    //Taurus
+    if (month == 4 && (day >= 20 && day <= 30))
+    {
+        cout << "The Taurus zodiac sign will have a busy 2019, working on repairing personal and professional relationships. This is the year when you need to make up for failed commitments. Make up for them before it is too late and it comes back to haunt you in the future. Relations with spouse will be sexually passionate for the bulls in 2019. Maybe planning for a child is a good idea. And remember, there is no time to be lazy this year!";
+        system("PAUSE");
+    }
+    //Taurus
+    if (month == 5 && (day >= 1 && day <= 20))
+    {
+        cout << "The Taurus zodiac sign will have a busy 2019, working on repairing personal and professional relationships. This is the year when you need to make up for failed commitments. Make up for them before it is too late and it comes back to haunt you in the future. Relations with spouse will be sexually passionate for the bulls in 2019. Maybe planning for a child is a good idea. And remember, there is no time to be lazy this year!";
+        system("PAUSE");
+    }
+    //Gemini
+    if (month == 5 && (day >= 21 && day <= 30))
+    {
+        cout << "2019 forecasts for the Gemini sun sign suggest that this year will be full of life and exuberance. You will be ready to try out something new all the time. This will make your life refreshing and a lot of fun. But along with starting something new, also be prepared to finish what you start. This is something that can put you under a little stress. But with proper management, your relationships, career, money and life can turn out for the best.";
+        system("PAUSE");
+    }
+    //Gemini
+    if (month == 6 && (day >= 1 && day <= 21))
+    {
+        cout << "2019 forecasts for the Gemini sun sign suggest that this year will be full of life and exuberance. You will be ready to try out something new all the time. This will make your life refreshing and a lot of fun. But along with starting something new, also be prepared to finish what you start. This is something that can put you under a little stress. But with proper management, your relationships, career, money and life can turn out for the best.";
+        system("PAUSE");
+    }
+    //Cancer
+    if (month == 6 && (day >= 22 && day <= 30))
+    {
+        cout << "The Cancer zodiac sign in 2019 will strive to bring simplicity in their lives this year. Remember, the decisions that you make in 2019 will lay the foundation for the coming years. Be it your job or relationships; think wisely before making important choices. Changes are a part of life. Embrace them before it is too late. Finances will be good for the crab in 2019. Proper fitness and diet are essential for your overall well being. ";
+        system("PAUSE");
+    }
+    //Cancer
+    if (month == 7 && (day >= 1 && day <= 23))
+    {
+        cout << "The Cancer zodiac sign in 2019 will strive to bring simplicity in their lives this year. Remember, the decisions that you make in 2019 will lay the foundation for the coming years. Be it your job or relationships; think wisely before making important choices. Changes are a part of life. Embrace them before it is too late. Finances will be good for the crab in 2019. Proper fitness and diet are essential for your overall well being. ";
+        system("PAUSE");
+    }
+    //Leo
+    if (month == 7 && (day >= 24 && day <= 31))
+    {
+        cout << "The birthday horoscope 2019 predicts that this will be a year of temptations for the Leo star sign. Everyone knows you put in a lot of efforts in your love life as well as professional commitments. But sometimes the thin line between right and wrong might be invisible to you. The lions have to be extra careful in 2019, if they do not wish to get caught in a web of deceit. Team work is particularly important for your success this year.";
+        system("PAUSE");
+    }
+    //Leo
+    if (month == 8 && (day >= 1 && day <= 23))
+    {
+        cout << "The birthday horoscope 2019 predicts that this will be a year of temptations for the Leo star sign. Everyone knows you put in a lot of efforts in your love life as well as professional commitments. But sometimes the thin line between right and wrong might be invisible to you. The lions have to be extra careful in 2019, if they do not wish to get caught in a web of deceit. Team work is particularly important for your success this year.";
+        system("PAUSE");
+    }
+    //Virgo
+    if (month == 8 && (day >= 24 && day <= 31))
+    {
+        cout << "The Virgo zodiac people will be very accurate and analytical in 2019. This is a year when you need to play on your strengths and make the most of your life instead of worrying about your past failures.  You might be better at working alone, but 2019 is a year of social interaction. Be it family gatherings or office parties, mingling with people will work in the Virgins’ favor. And it is an excellent idea to go on that vacation you have been planning on for some time.";
+        system("PAUSE");
+    }
+    //Virgo
+    if (month == 9 && (day >= 1 && day <= 22))
+    {
+        cout << "The Virgo zodiac people will be very accurate and analytical in 2019. This is a year when you need to play on your strengths and make the most of your life instead of worrying about your past failures.  You might be better at working alone, but 2019 is a year of social interaction. Be it family gatherings or office parties, mingling with people will work in the Virgins’ favor. And it is an excellent idea to go on that vacation you have been planning on for some time.";
+        system("PAUSE");
+    }
+    //Libra
+    if (month == 9 && (day >= 23 && day <= 30))
+    {
+        cout << "2019 will be all about balance for the Libra personality. The zodiac signs predictions for 2019 forecast that you might even be lucky in love this year. You just need to recognize the right partner. Creativity on the work front will set you up for many laurels. And when it comes to money, be sure not to spend beyond your means. This will be a relaxing year for the Libra star sign. And be prepared to face some interesting and life changing situations in 2019.";
+        system("PAUSE");
+    }
+    //Libra
+    if (month == 10 && (day >= 1 && day <= 22))
+    {
+        cout << "2019 will be all about balance for the Libra personality. The zodiac signs predictions for 2019 forecast that you might even be lucky in love this year. You just need to recognize the right partner. Creativity on the work front will set you up for many laurels. And when it comes to money, be sure not to spend beyond your means. This will be a relaxing year for the Libra star sign. And be prepared to face some interesting and life changing situations in 2019.";
+        system("PAUSE");
+    }
+    //Scorpio
+    if (month == 10 && (day >= 23 && day <= 31))
+    {
+        cout << "The 2019 zodiac predictions forecast a dramatic year for the Scorpio zodiac sign. Whether it will be good or bad will entirely depend on the actions of the Scorpions. This is an auspicious year to look for a new job or start a new business venture. Also you will be the one who will be responsible for making or breaking your life. Do not let arguments and conflicts ruin your romance. Be mature enough to handle all such issues with a firm hand.";
+        system("PAUSE");
+    }
+    //Scorpio
+    if (month == 11 && (day >= 1 && day <= 22))
+    {
+        cout << "The 2019 zodiac predictions forecast a dramatic year for the Scorpio zodiac sign. Whether it will be good or bad will entirely depend on the actions of the Scorpions. This is an auspicious year to look for a new job or start a new business venture. Also you will be the one who will be responsible for making or breaking your life. Do not let arguments and conflicts ruin your romance. Be mature enough to handle all such issues with a firm hand.";
+        system("PAUSE");
+    }
+    //Sagittarius
+    if (month == 11 && (day >= 23 && day <= 30))
+    {
+        cout << "The 2019 Sagittarius horoscope predictions foretell that this will be a year for changes and overcoming your inhibitions. In 2019, you will have the freedom to do what you have always been aiming for. There will be no restrictions from anyone or anything whatsoever. But this also means that you understand your responsibilities and stay within your limits. Focus, persevere and aim for your goals with an open mind and success will definitely be yours.";
+        system("PAUSE");
+    }
+    //Sagittarius
+    if (month == 12 && (day >= 1 && day <= 21))
+    {
+        cout << "The 2019 Sagittarius horoscope predictions foretell that this will be a year for changes and overcoming your inhibitions. In 2019, you will have the freedom to do what you have always been aiming for. There will be no restrictions from anyone or anything whatsoever. But this also means that you understand your responsibilities and stay within your limits. Focus, persevere and aim for your goals with an open mind and success will definitely be yours.";
+        system("PAUSE");
+    }
+    //Capricorn
+    if (month == 12 && (day >= 22 && day <= 31))
+    {
+        cout << "The astrology 2019 predictions for the Capricorn zodiac sign forecast a year of progress and prosperity. This is the year when the sea goats should work hard and plan well for the future. Whatever you touch will turn to gold. So make the most of all opportunities this year. Learn to appreciate your partner instead of being a dominating lover all the time. Even in your career, it is time to start working as a team.";
+        system("PAUSE");
+    }
+    //Capricorn
+    if (month == 1 && (day >= 1 && day <= 20))
+
+    {
+        cout << "The astrology 2019 predictions for the Capricorn zodiac sign forecast a year of progress and prosperity. This is the year when the sea goats should work hard and plan well for the future. Whatever you touch will turn to gold. So make the most of all opportunities this year. Learn to appreciate your partner instead of being a dominating lover all the time. Even in your career, it is time to start working as a team.";
+        system("PAUSE");
+    }
+    else
+    {
+        cout << "Please insert a valid answer.";
+    }
+}
