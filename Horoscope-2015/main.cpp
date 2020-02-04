d47b782b062f094f2c5a806b2f6035e69ea7293d
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-04 17:56:02 -0500

    Update main.cpp

--- Horoscope-2015/main.cpp
+++ Horoscope-2015/main.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main(){
+    int month;
+    int day;
     cout << "Horoscope 2015 by Kyle Santiago\n";
     cout << "Horoscope information provided by astrology.horoscope.com\n\n";
     cout << "1: January\n2: February\n3: March\n4: April\n5: May\n6: June\n7: July\n8: August\n9: September\n10: October\n11: November\n12: December\n";
-    int month;
     cout << "\nPlease enter the month you were born in: ";
     cin >> month;
     cout << "Please enter the day you were born: ";
-    int day;
     cin >> day;
     cout << "\nHoroscope \n\n";
     //Aquarius
